2bf665519c2cb59613a7d918e8ad35a43946459f
romualdo.andre <romualdo.andre@sec.ba.gov.br>
2024-12-23 18:16:00 -0300

    Início do cadastro de produtos

---
+++ src/components/produtos/cadastro/index.tsx
@@ -0,0 +1,11 @@
+import {Layout} from 'components'
+
+const CadastroProdutos: React.FC = ()=>{
+    return (
+        <Layout titulo='Cadastro de Produtos'>
+
+        </Layout>
+    )
+}
+
+export default CadastroProdutos;
