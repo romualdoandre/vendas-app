89462aa2e1ad813c31b3fa6d4fea0656624ed235
romualdo.andre <romualdo.andre@sec.ba.gov.br>
2024-12-26 09:47:43 -0300

    formatação do formulário de cadastro

--- src/components/produtos/cadastro/index.tsx
+++ src/components/produtos/cadastro/index.tsx
@@ -1,54 +1,66 @@
-import {Layout} from 'components'
+import { Layout } from 'components'
 
-export const CadastroProdutos: React.FC = ()=>{
+export const CadastroProdutos: React.FC = () => {
     return (
         <Layout titulo='Produtos'>
-            <div className='field'>
-                <label className='label' htmlFor='inputSku'>SKU: *</label>
-                <div className='control'>
-                    <input className='input'
-                    id='inputSku'
-                    placeholder='Digite o SKU do produto'>
-                    </input>
-                </div>
+            <div className='columns'>
+                <div className='column is-half'>
+                    <div className='field'>
+                        <label className='label' htmlFor='inputSku'>SKU: *</label>
+                        <div className='control'>
+                            <input className='input'
+                                id='inputSku'
+                                placeholder='Digite o SKU do produto'>
+                            </input>
+                        </div>
 
-            </div>
-            <div className='field'>
-                <label className='label' htmlFor='inputPreco'>Preço: *</label>
-                <div className='control'>
-                    <input className='input'
-                    id='inputPreco'
-                    placeholder='Digite o preço do produto'>
-                    </input>
+                    </div>
                 </div>
+                <div className='column is-half'>
+                    <div className='field'>
+                        <label className='label' htmlFor='inputPreco'>Preço: *</label>
+                        <div className='control'>
+                            <input className='input'
+                                id='inputPreco'
+                                placeholder='Digite o preço do produto'>
+                            </input>
+                        </div>
 
-            </div>
-            <div className='field'>
-                <label className='label' htmlFor='inputNome'>Nome: *</label>
-                <div className='control'>
-                    <input className='input'
-                    id='inputNome'
-                    placeholder='Digite o nome do produto'>
-                    </input>
+                    </div>
                 </div>
+            </div>
+            <div className='columns'>
+                <div className='field column'>
+                    <label className='label' htmlFor='inputNome'>Nome: *</label>
+                    <div className='control'>
+                        <input className='input'
+                            id='inputNome'
+                            placeholder='Digite o nome do produto'>
+                        </input>
+                    </div>
 
-            </div>
-            <div className='field'>
-                <label className='label' htmlFor='inputDescricao'>Descrição: *</label>
-                <div className='control'>
-                    <textarea className='textarea'
-                    id='inputDescricao'
-                    placeholder='Digite a descrição do produto'>
-                    </textarea>
                 </div>
+            </div>
+            <div className='columns'>
+                <div className='field column'>
+                    <label className='label' htmlFor='inputDescricao'>Descrição: *</label>
+                    <div className='control'>
+                        <textarea className='textarea'
+                            id='inputDescricao'
+                            placeholder='Digite a descrição do produto'>
+                        </textarea>
+                    </div>
 
-            </div>
-            <div className='field is-grouped'>
-                <div className='control'>
-                    <button className='button is-link'>Salvar</button>
                 </div>
-                <div className='control'>
-                    <button className='button is-link is-light'>Cancelar</button>
+            </div>
+            <div className='columns'>
+                <div className='field is-grouped column'>
+                    <div className='control'>
+                        <button className='button is-link'>Salvar</button>
+                    </div>
+                    <div className='control'>
+                        <button className='button is-link is-light'>Cancelar</button>
+                    </div>
                 </div>
             </div>
         </Layout>
