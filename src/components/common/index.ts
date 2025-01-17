56f33511b72ccf84c827694b62699c870f41a49a
romualdo.andre <romualdo.andre@sec.ba.gov.br>
2025-01-17 16:14:58 -0300

    listagem de produtos

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,2 +1,3 @@
 export { Message } from "./message";
 export { Input } from "./input";
+export { Loader } from './loader'
--- src/components/produtos/listagem/index.tsx
+++ src/components/produtos/listagem/index.tsx
@@ -1,10 +1,33 @@
 import { Layout } from 'components';
 import Link from 'next/link';
+import { TabelaProdutos } from './tabela';
+import { Produto } from 'app/models/produtos';
+import useSWR from 'swr'
+import { httpClient } from 'app/http';
+import { Axios, AxiosResponse } from 'axios';
+import { useEffect } from 'react'
+import { useState } from 'react'
+import { Alert } from 'components/common/message'
+import { useProdutoService } from 'app/services';
+import { Loader } from 'components/common';
+
+
 
 export const ListagemProdutos: React.FC = () => {
+    const { data: result, error } = useSWR<AxiosResponse<Produto[]>>
+                    ('/api/produtos', url => httpClient.get(url) )
+
+    const [ lista, setLista ] = useState<Produto[]>([])
+    const service = useProdutoService()
+    useEffect( () => {
+        setLista(result?.data || [])
+    }, [result])
+
     return (<Layout titulo='Produtos'>
         <Link href="/cadastros/produtos">
             <button className='button is-warning'>Novo</button>
         </Link>
+        <br/><Loader show={!result} />
+        <TabelaProdutos produtos={lista}></TabelaProdutos>
     </Layout>)
 }
