2c7a8c117c1c472122026ed7399a97ce2c9e8c48
romualdo.andre <romualdo.andre@sec.ba.gov.br>
2025-01-27 16:21:53 -0300

    campos de consulta de clientes

--- src/components/clientes/listagem/index.tsx
+++ src/components/clientes/listagem/index.tsx
@@ -1,8 +1,42 @@
 import { Layout } from "components"
+import { Input, InputCPF } from "components"
+import { useFormik } from "formik"
+
+interface ConsultaClientesForm{
+    nome?: string;
+    cpf?: string;
+}
 
 export const ListagemClientes: React.FC = () => {
+    const handleSubmit = (filtro: ConsultaClientesForm) =>{
+        console.log(filtro)
+    }
+    const formik = useFormik<ConsultaClientesForm>({
+        onSubmit: handleSubmit,
+        initialValues: {nome:'', cpf: ''}
+    })
+
+    
 
     return (
-        <Layout titulo="Clientes"></Layout>
+        <Layout titulo="Clientes">
+            <form onSubmit={formik.handleSubmit}>
+                <div className="columns">
+                    <Input label="Nome:" id="nome" name="nome"
+                     value={formik.values.nome} columnClasses="is-half"
+                     autoComplete="off" onChange={formik.handleChange}></Input>
+                    <InputCPF label="CPF:" id="cpf" name="cpf"
+                     value={formik.values.cpf} columnClasses="is-half"
+                     onChange={formik.handleChange}></InputCPF>
+                </div>
+                <div className="field is-grouped">
+                    <div className="control is-link">
+                        <button type="submit" className="button is-success">
+                            Consultar
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </Layout>
     )
 }
